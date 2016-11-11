2f493a2ab0d662ae43a3ed52f1a102fa8018d317
vicowara <mm.souichi@gmail.com>
2016-11-11 16:00:33 +0900

    add orgtbl mode for pandoc

--- init.el
+++ init.el
@@ -526,4 +526,16 @@
 
 (setq enable-remote-dir-locals 1)
 
+(require 'org-table)
+(defun orgtbl-to-gfm (table params)
+  "Convert the Orgtbl mode TABLE to GitHub Flavored Markdown."
+  (let* ((alignment (mapconcat (lambda (x) (if x "|--:" "|---"))
+                               org-table-last-alignment ""))
+         (params2
+          (list
+           :splice t
+           :hline (concat alignment "|")
+           :lstart "| " :lend " |" :sep " | ")))
+    (orgtbl-to-generic table (org-combine-plists params2 params))))
+
 ;;
