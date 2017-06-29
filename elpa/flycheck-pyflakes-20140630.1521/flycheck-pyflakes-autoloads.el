20479e7bbff402a34cc59ca6dcd09d7c7a3b8211
vicowara <mm.souichi@gmail.com>
2017-06-29 21:16:13 +0900

    update elpa

--- elpa/flycheck-pyflakes-20140630.1521/flycheck-pyflakes-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; flycheck-pyflakes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("flycheck-pyflakes-pkg.el" "flycheck-pyflakes.el")
-;;;;;;  (21580 52114 790659 0))
-
-;;;***
-
-(provide 'flycheck-pyflakes-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flycheck-pyflakes-autoloads.el ends here
--- elpa/shell-pop-20170223.222/shell-pop-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "shell-pop" "20170223.222" "helps you to use shell easily on Emacs. Only one key action to work." '((emacs "24") (cl-lib "0.5")) :url "http://github.com/kyagi/shell-pop-el" :keywords '("shell" "terminal" "tools"))
