ef1a0503613c3aa2a67f56b84e4ec86b2021655e
vicowara <mm.souichi@gmail.com>
2017-06-29 21:15:16 +0900

    update elpa

---
+++ elpa/ddskk-20160820.1428/ddskk-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ddskk" "20160820.1428" "Simple Kana to Kanji conversion program."
+  '((ccc "1.43")
+    (cdb "20141201.754")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/pandoc-20160607.1010/pandoc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pandoc" "20160607.1010" "Pandoc interface" '((emacs "24.4")) :url "https://github.com/zonuexe/pandoc.el" :keywords '("documentation" "markup" "converter"))
