4b2b59b6cba5321cf5831558717f84efeae1057e
vicowara <mm.souichi@gmail.com>
2018-04-20 09:02:42 +0900

    remove libraries

--- init.el
+++ init.el
@@ -9,6 +9,12 @@
 ;; package.elの初期化
 (package-initialize)
 
+;; package-selected-packagesが必要なので先にcustom.elを読み込む
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; package-selected-packagesで指定されたパッケージをインストール(初回時を想定)
 (setq package-pinned-packages
       '((web-mode . "melpa-stable")
         (auto-complete . "melpa-stable")))
@@ -21,27 +27,3 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits/")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(google-translate-default-source-language "en")
- '(google-translate-default-target-language "ja")
- '(helm-gtags-auto-update t)
- '(helm-gtags-ignore-case t)
- '(irony-additional-clang-options (quote ("-std=c++11")))
- '(irony-cdb-compilation-databases
-   (quote
-	(irony-cdb-libclang irony-cdb-clang-complete irony-cdb-json)))
- '(package-selected-packages
-   (quote
-	(auto-virtualenvwrapper virtualenvwrapper ace-jump-mode helm-swoop ace-isearch helm-migemo flycheck-popup-tip flycheck-mypy flycheck-pycheckers srefactor kivy-mode helm-tramp yasnippet-snippets yatex pcap-mode flycheck-clang-analyzer company-irony-c-headers flycheck-irony company company-c-headers company-irony company-jedi irony volatile-highlights undohist twittering-mode tabbar sticky sr-speedbar smartrep smartparens shell-pop semi scala-mode python-mode pyflakes popwin php-mode php-eldoc pcre2el pandoc-mode pandoc multi-term mode-compile migemo markdown-mode magit init-loader helm-gtags helm-flymake helm-flycheck helm-cscope helm-ag haskell-mode gtags graphviz-dot-mode google-translate go-mode ggtags foreign-regexp flycheck-pyflakes expand-region exec-path-from-shell django-mode ddskk codic bison-mode auto-install auto-complete-clang anzu ace-link ac-helm yasnippet)))
- '(tab-width 4))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
